6994bc2589f67a10991e39bc32254db5ea7dccad
handwoo24 <81428531+handwoo24@users.noreply.github.com>
2021-06-17 22:46:00 +0900

    모듈 임포트 as 사용해보기

--- typescript/ch02-2/src/person/Person.ts
+++ typescript/ch02-2/src/person/Person.ts
@@ -1,4 +1,4 @@
-let MAX_AGE = 100
+import * as U from '../utils/makeRandomNumber'
 
 export interface Iperson {
     name: string
@@ -10,10 +10,6 @@ class Person implements Iperson {
     constructor(public name: string, public age: number) {}
 }
 
-function makeRandomNumber(max: number = MAX_AGE): number {
-    return Math.ceil((Math.random() * max))
-}
-
 export const makePerson = (name: string,
-    age:number = makeRandomNumber()) => ({name, age})
+    age:number = U.makeRandomNumber()) => ({name, age})
     //화살표 함수는 리액트 잠깐 보면서 처음 본건데, 아직도 헷갈린다. 그러니까 이게 할당한다는 거였던 거 같은데.
