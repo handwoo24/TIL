d7f6f01e42af4cafd1d5d8619bca6c43426e85c7
seungwoo Ubuntu <handwoo24.gmail.com>
2021-07-18 13:23:41 +0900

    🔨완료!

--- Quiz/black_jack.py
+++ Quiz/black_jack.py
@@ -21,7 +21,40 @@ class Dealer:
         return random.randrange(3, 100)
 
 class BlackJackMachine:
-    pass
+    _selection: int = 3
+
+
+    def game(self, sum_of_card_number: int, card_list: List[int]) -> int:
+        cards: List[int] = []
+        print("input list: ", card_list)
+        print("input sum: ", sum_of_card_number)
+        play_number = self._combination(len(card_list))
+        print("play numer: ", play_number)
+        result: int = sum_of_card_number
+        for i in range(0, play_number):
+            copy_list = list(card_list)
+            for j in range(0, 3):
+                index = random.randrange(0, len(copy_list))
+                card = copy_list[index]
+                copy_list.remove(card)
+                cards.append(card)
+            
+            card_sum = sum(cards)
+            print(card_sum)
+            current_result = sum_of_card_number - card_sum
+            if current_result < result and card_sum <= sum_of_card_number:
+                result = current_result
+                sum_result = card_sum
+
+            cards = []
+        
+        return result, sum_result
+
+    def _combination(self, length) -> int:
+        denominator = length * (length - 1) * (length - 2)
+        numerator = 6
+        situation = int(denominator / numerator)
+        return situation * 10
 
 def main():
     dealer = Dealer()
@@ -30,7 +63,8 @@ def main():
     card_list: List[int] = dealer.shuffle(number_cards)
 
     black_jack_machine = BlackJackMachine()
-    pass
+    result, sum = black_jack_machine.game(sum_of_card_number, card_list)
+    print(result, sum)
 
 if __name__ == "__main__":
     main()
