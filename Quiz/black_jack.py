4a27012b312b511fe7b86997cc08ebb4f83d2cbb
seungwoo Ubuntu <handwoo24.gmail.com>
2021-07-18 13:20:32 +0900

    🔨shuffle method 완료

--- Quiz/black_jack.py
+++ Quiz/black_jack.py
@@ -4,7 +4,13 @@ import random
 
 class Dealer:
     def shuffle(self, number_cards: int) -> List[int]:
-        pass
+        card_list: list = []
+        for i in range(0, number_cards):
+            card_number = random.randrange(0, 100000)
+            card_list.append(card_number)
+        
+        return card_list
+        
     pass
 
 def main():
